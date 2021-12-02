871599165ace0b6ba36f5bda2a39d42c14c708f0
Bryon Smith <bryon@smithauto.us>
2021-12-01 19:38:38 -0600

    challenge1

--- day1/challenge1.mjs
+++ day1/challenge1.mjs
@@ -1,14 +1,13 @@
-import { sweeps } from './sweep.mjs'
-;(async function () {
-  // 1. How many measurements are larger than the previous measurement?
-  let sweepCount = 0
-  sweeps.reduce((acc, cur, i) => {
-    if (i === 0) return cur
-    if (cur > acc) {
-      sweepCount++
-      return cur
-    }
-    return cur
-  }, 0)
-  console.log(sweepCount)
-})()
+import * as fs from 'fs'
+
+const sweeps = fs
+  .readFileSync('./day1/sweeps.txt', 'utf-8')
+  .split(/\r?\n/)
+  .map((num) => +num)
+
+// Challenge 1. How many measurements are larger than the previous measurement?
+const sweepCount = sweeps.reduce(
+  (acc, cur, i) => ((sweeps[i - 1] || Infinity) < cur ? acc + 1 : acc),
+  0
+)
+console.log(sweepCount) //1446
