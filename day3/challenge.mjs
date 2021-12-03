143aa405ac188c2cb15537659aaa76b8e6d0262f
Bryon Smith <bryon@smithauto.us>
2021-12-03 16:46:57 -0600

    remove comments

--- day3/challenge.mjs
+++ day3/challenge.mjs
@@ -1,10 +1,6 @@
 import * as fs from 'fs'
 /*
 Day 3, Challenge 1: Power Consumption
-    Each binary number in report will be used to generate two numbers (gamma, epsilon).
-        Gamma: the most common bit, (1, 0) starting from left [0], count if one or zero.
-        Epsilon: least common bit (opposite of Gamma)
-    Convert to decimal and multiply both numbers to get power consumption.
 */
 const binaries = fs.readFileSync('./day3/binary.txt', 'utf-8').split(/\r?\n/)
 const lineLength = binaries[0].length
@@ -35,15 +31,6 @@ console.log(
 
 /*
 Day3, Challenge 2: Life Support
-  Find oxygen rating:
-    Starting left from [0] Find the most common bit (1, 0) of all the binary data.
-    Keep the new binary data and find most common bit in position 2 of new binary data.
-    Continue through all chars, till final number is reached.
-
-  Find C02 Scrubber rating:
-    same as oxygen, except find least most common.
-
-  return oxygen rating * co2 scrubber rating
 */
 
 function getLifeSupport(binary, type, n, i = 0) {
