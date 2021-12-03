2abe1d8910928d4601854575c11fd4bc4695dd50
Bryon Smith <bryon@smithauto.us>
2021-12-03 17:03:50 -0600

    remove empty lines

--- day3/challenge.mjs
+++ day3/challenge.mjs
@@ -1,37 +1,26 @@
 import * as fs from 'fs'
-/*
-Day 3, Challenge 1: Power Consumption
-*/
+// Day 3, Challenge 1: Power Consumption 
 const binaries = fs.readFileSync('./day3/binary.txt', 'utf-8').split(/\r?\n/)
 const lineLength = binaries[0].length
 
 // return most common bit
 function getMostCommonBit(binary, type, i) {
-  if (type === 'gamma' || type === 'oxygen') {
-    return binary.filter((bin) => bin[i] === '1').length / binary.length >= 0.5
-      ? '1'
-      : '0'
-  } else {
-    return binary.filter((bin) => bin[i] === '1').length / binary.length < 0.5
-      ? '1'
-      : '0'
-  }
+  return (type === 'gamma' || type === 'oxygen') 
+    ? binary.filter((bin) => bin[i] === '1').length / binary.length >= 0.5 ? '1' : '0'
+    : binary.filter((bin) => bin[i] === '1').length / binary.length < 0.5 ? '1' : '0'
 }
+
 // recursion: return number
 function getPowerRating(binary, type, n, i = 0, p = '') {
   if (n === 0) return parseInt(p, 2)
   p = p + getMostCommonBit(binary, type, i)
   return getPowerRating(binary, type, n - 1, i + 1, p)
 }
-console.log(
-  'Power Rating: ',
-  getPowerRating(binaries, 'gamma', lineLength) *
-    getPowerRating(binaries, 'epsilon', lineLength)
-) // testBinary.txt: 198, binary.txt: 738234
+const powerRating = getPowerRating(binaries, 'gamma', lineLength) * getPowerRating(binaries, 'epsilon', lineLength)
+console.log(powerRating) // testBinary.txt: 198, binary.txt: 738234
 
-/*
-Day3, Challenge 2: Life Support
-*/
+//
+// Day3, Challenge 2: Life Support -----------------------------------------------------------------
 function getLifeSupport(binary, type, n, i = 0) {
   if (n === 0) return parseInt(binary.join(''), 2)
   if (binary.length === 1) return parseInt(binary.join(''), 2)
@@ -40,8 +29,5 @@ function getLifeSupport(binary, type, n, i = 0) {
   )
   return getLifeSupport(binary, type, n - 1, i + 1)
 }
-console.log(
-  'Life Support: ',
-  getLifeSupport(binaries, 'oxygen', lineLength) *
-    getLifeSupport(binaries, 'c02', lineLength)
-) // testBinary: 230 , binary: 3969126
+const lifeSupport = getLifeSupport(binaries, 'oxygen', lineLength) * getLifeSupport(binaries, 'c02', lineLength)
+console.log(lifeSupport) // testBinary: 230 , binary: 3969126
